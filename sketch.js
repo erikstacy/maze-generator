f240609bdae75fead789554ca6e28f229b8fe789
erikstacy <erikslaststand@gmail.com>
2020-03-26 12:56:06 -0500

    Part 2 of the video

--- sketch.js
+++ sketch.js
@@ -5,12 +5,18 @@ var rows;
 var w = 40;		// Width and height of each square
 var grid = [];
 
+// Current cell
+var current;
+
 function setup() {
 	createCanvas(600, 600);
 
 	// Creating the size of the column and rows
 	cols = floor(width / w);
 	rows = floor(height / w);
+
+	// Set the framerate
+	frameRate(5);
 
 	// Initialize the grid with cell objects
 	for (var j = 0; j < rows; j++) {
@@ -19,6 +25,9 @@ function setup() {
 			grid.push(cell);
 		}
 	}
+
+	// Starting the maze at the top left
+	current = grid[0];
 }
 
 function draw() {
@@ -28,12 +37,69 @@ function draw() {
 	for (var i = 0; i < grid.length; i++) {
 		grid[i].show();
 	}
+
+	// Start by visiting the current cell
+	current.visited = true;
+
+	// Check the current node's neighbors
+	// 		If any of the neighbors haven't been visited, grab
+	//		one neighbor at random
+	var next = current.checkNeighbors();
+
+	// Visit this next cell
+	if (next) {
+		next.visited = true;
+		current = next;
+	}
+}
+
+// Magic formula to get 1D array position with i and j
+function index(i, j) {
+	// If the index is an edge case, return -1
+	if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+		return -1;
+	}
+
+	return i + j * cols;
 }
 
 function Cell(i, j) {
 	this.i = i;
 	this.j = j;
 	this.walls = [true, true, true, true];	// Top, Right, Bottom, Left
+	this.visited = false;
+
+	this.checkNeighbors = function() {
+		var neighbors = [];
+
+		// Get the cell's neighbors
+		var top = grid[index(i, j-1)];
+		var right = grid[index(i+1, j)];
+		var bottom = grid[index(i, j+1)];
+		var left = grid[index(i-1, j)];
+
+		// If the neighbor hasn't been visited, push it to the neighbors array
+		if (top && !top.visited) {
+			neighbors.push(top);
+		}
+		if (right && !right.visited) {
+			neighbors.push(right);
+		}
+		if (bottom && !bottom.visited) {
+			neighbors.push(bottom);
+		}
+		if (left && !left.visited) {
+			neighbors.push(left);
+		}
+
+		// If we have unvisited neighbors, grab a random neighbor
+		if (neighbors.length > 0) {
+			var r = floor(random(0, neighbors.length));
+			return neighbors[r];
+		} else {
+			return undefined;
+		}
+	}
 
 	this.show = function() {
 		var x = this.i * w;
@@ -52,6 +118,11 @@ function Cell(i, j) {
 		}
 		if (this.walls[3]) {
 			line(x, y+w, x, y);
+		}
+
+		if (this.visited) {
+			fill(255, 0, 255, 100);
+			rect(x, y, w, w);
 		}
 	}
 }
