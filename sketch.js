5b426b77560291f4171fa566448c4671429c542a
erikstacy <erikslaststand@gmail.com>
2020-03-26 13:28:32 -0500

    Part 3 of the video

--- sketch.js
+++ sketch.js
@@ -40,6 +40,7 @@ function draw() {
 
 	// Start by visiting the current cell
 	current.visited = true;
+	current.highlight();
 
 	// Check the current node's neighbors
 	// 		If any of the neighbors haven't been visited, grab
@@ -48,7 +49,13 @@ function draw() {
 
 	// Visit this next cell
 	if (next) {
+		// Mark next as visited
 		next.visited = true;
+
+		// Remove the walls between current and next
+		removeWalls(current, next);
+
+		// Next cell is now the current cell
 		current = next;
 	}
 }
@@ -121,8 +128,39 @@ function Cell(i, j) {
 		}
 
 		if (this.visited) {
+			noStroke();
 			fill(255, 0, 255, 100);
 			rect(x, y, w, w);
 		}
+	}
+
+	this.highlight = function() {
+		var x = this.i * w;
+		var y = this.j * w;
+		noStroke();
+		fill(0, 0, 255, 100);
+		rect(x, y, w, w);
+	}
+}
+
+function removeWalls(a, b) {
+	// Check where the cells are in relation to each other
+	// and remove the walls between the cells
+	var x = a.i - b.i;
+	if (x === 1) {
+		a.walls[3] = false;
+		b.walls[1] = false;
+	} else if (x === -1) {
+		a.walls[1] = false;
+		b.walls[3] = false;
+	}
+
+	var y = a.j - b.j;
+	if (y === 1) {
+		a.walls[0] = false;
+		b.walls[2] = false;
+	} else if (y === -1) {
+		a.walls[2] = false;
+		b.walls[0] = false;
 	}
 }
